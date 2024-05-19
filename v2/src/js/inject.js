80aa235c53978589e4a58d34c2d29b26031437f3
tracy-l-a <tracy.la829@gmail.com>
2024-05-18 17:12:11 -0700

    making checkbox invisible

--- v2/src/js/inject.js
+++ v2/src/js/inject.js
@@ -1,19 +1,14 @@
 // alert("hello, world");
 
-<<<<<<< HEAD
-=======
 const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
 
 function changeCheckbox() {
     checkboxes.forEach(function (checkbox) {
         if (checkboxes.checked) {
-            alert("checked");
-        } else {
-            alert("You didn't check it! Let me check it for you.");
+            checkbox.style.opacity = '0';
         }
     });
 }
 
 changeCheckbox();
->>>>>>> 9c3deb460fb27aa9a77b962c5523761b8e22e15d
