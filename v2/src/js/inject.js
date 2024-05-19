9c3deb460fb27aa9a77b962c5523761b8e22e15d
tracy-l-a <tracy.la829@gmail.com>
2024-05-18 17:00:09 -0700

    added checkbox change

--- v2/src/js/inject.js
+++ v2/src/js/inject.js
@@ -1,1 +1,16 @@
 // alert("hello, world");
+
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+
+function changeCheckbox() {
+    checkboxes.forEach(function (checkbox) {
+        if (checkboxes.checked) {
+            alert("checked");
+        } else {
+            alert("You didn't check it! Let me check it for you.");
+        }
+    });
+}
+
+changeCheckbox();
